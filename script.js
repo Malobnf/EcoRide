5e87c19f5ceedd2b3746acb40b5889d7e9d20536
Malobnf <maloburnouf@gmail.com>
2025-07-01 09:48:45 +0200

    Modification header + menu déroulant, création de la page de profil

--- script.js
+++ script.js
@@ -12,3 +12,17 @@ function openNav() {
 function closeNav() {
   sidenav.classList.remove("active");
 }
+
+function toggleMenu() {
+  const menu = document.getElementById('sideMenu');
+  menu.style.display = (menu.style.display === 'flex') ? 'none' : 'flex';
+}
+
+// Ferme le menu si on clique ailleurs
+document.addEventListener('click', function (e) {
+  const menu = document.getElementById('sideMenu');
+  const button = document.querySelector('.menu-button');
+  if (!menu.contains(e.target) && !button.contains(e.target)) {
+    menu.style.display = 'none';
+  }
+});
