1be8c6748b0fd6370ede2684673e4aabaebab9fa
Malo Burnouf <maloburnouf@gmail.com>
2025-07-20 14:59:28 +0200

    Ajout de composer.lock

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '3457f8a9906aa9e6c6eeea7b99dacf98fadc1518',
+        'name' => 'malo/ecoride',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'malo/ecoride' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '3457f8a9906aa9e6c6eeea7b99dacf98fadc1518',
+            'dev_requirement' => false,
+        ),
+    ),
+);
