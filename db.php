04b8e6022865dd56a68af58995a84613e60ec74b
Malo Burnouf <maloburnouf@gmail.com>
2025-07-21 16:34:42 +0200

    Idem

--- db.php
+++ db.php
@@ -1,17 +1,17 @@
-<!-- Connexion postgre -->
 <?php
-$host = getenv("DB_HOST");
-$port = getenv("DB_PORT");
-$dbname = getenv("DB_NAME");
-$user = getenv("DB_USER");
-$pass = getenv("DB_PASSWORD");
+$host = 'etdq12exrvdjisg6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+$port = '3306';
+$dbname = 'dbtihpk3ml0inl13';
+$user = 'hyig8y2kcbjlzzj4';
+$pass = 'q58gn7sukb56s3en';
 
-$dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
+$dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4";
 
 try {
     $pdo = new PDO($dsn, $user, $pass, [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ]);
+    echo "✅ Connexion réussie à JawsDB MySQL !";
 } catch (PDOException $e) {
-    die("Erreur de connexion à la base Supabase : " . $e->getMessage());
+    die("❌ Erreur de connexion à la base JawsDB : " . $e->getMessage());
 }
