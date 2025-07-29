27a8c6da0edcea323e0e34d8d7bddfaea15411fc
Malobnf <maloburnouf@gmail.com>
2025-07-29 10:30:29 +0200

    Maj CSSpage connexion/inscription, ajout formulaire de contact

---
+++ contact.js
@@ -0,0 +1,10 @@
+function submitmessage {
+    fetch ('contact.php', {
+        method: 'POST',
+        headers:'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    })
+    document.getElementById('contact-btn').onclick = () => {
+
+    }
+}
